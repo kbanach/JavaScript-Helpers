dc9590a28bf6c1b5bceb170a5861f216f172b720
kbanach <banach.krystian@gmail.com>
2016-03-25 13:16:16 +0100

    #12 Typescript!
    Added:
    * ignoring of compiled TS files (all .js and .js.map in /app)

--- app/_all.js
+++
@@ -1,5 +0,0 @@
-/// <reference path='../bower_components/DefinitelyTyped/angularjs/angular.d.ts' />
-/// <reference path='../bower_components/DefinitelyTyped/lodash/lodash.d.ts' />
-/// <reference path='scripts/controllers/main.ts' />
-/// <reference path='scripts/app.ts' /> 
-//# sourceMappingURL=_all.js.map
