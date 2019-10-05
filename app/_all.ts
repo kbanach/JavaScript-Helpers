3dbc29aec64a74c23870788cee35d9a5ca485c10
Krystian Banach <krystian.banach@ergo.digital>
2019-10-05 23:33:32 +0200

    Migrate to React
    remove everything

--- app/_all.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path='../bower_components/DefinitelyTyped/angular/index.d.ts' />
-/// <reference path='../bower_components/DefinitelyTyped/lodash/index.d.ts' />
-/// <reference path='scripts/controllers/main.ts' />
-/// <reference path='scripts/app.ts' />
