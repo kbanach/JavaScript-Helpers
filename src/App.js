cfb3203a054fee88741a3ca8db48811ac1246fdb
kbanach <krystian.banach@ergo.digital>
2019-10-17 17:17:02 +0200

    Migrate to React
    refactor: remove all unnecessary create-react-app garbage

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 import React from 'react';
 import './App.css';
-import { ThemeContext, themes } from './theme';
-import { Header } from './Header';
 
 class App extends React.Component {
-  state = {
-    theme: themes.dark,
-  };
-
-  changeTheme() {
-    return (e) => {
-      e.preventDefault();
-
-      this.setState({
-        ...this.state,
-        theme: (this.state.theme === themes.dark) ? themes.light : themes.dark,
-      });
-    }
-  }
 
   render() {
     return (
       <div className="App">
-        <ThemeContext.Provider value={this.state.theme}>
-          <Header />
-          <button onClick={this.changeTheme()}>Change theme</button>
-        </ThemeContext.Provider>
       </div>
     );
   }
