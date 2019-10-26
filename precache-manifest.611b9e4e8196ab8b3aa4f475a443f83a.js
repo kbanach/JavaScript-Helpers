6de8c2dd88be98cdad1a4e01fbe8fcd62c2096a3
kbanach <krystian.banach@ergo.digital>
2019-10-26 21:46:35 +0200

    update Readme and newest build

---
+++ precache-manifest.611b9e4e8196ab8b3aa4f475a443f83a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efcb449772179a13f3a298fb57c62782",
+    "url": "/kbanach/JavaScript-Helpers/index.html"
+  },
+  {
+    "revision": "e4baa2b65f74378e56f6",
+    "url": "/kbanach/JavaScript-Helpers/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "0f45801210bd281d03bf",
+    "url": "/kbanach/JavaScript-Helpers/static/css/main.7015c6b5.chunk.css"
+  },
+  {
+    "revision": "e4baa2b65f74378e56f6",
+    "url": "/kbanach/JavaScript-Helpers/static/js/2.7afd7ab5.chunk.js"
+  },
+  {
+    "revision": "0f45801210bd281d03bf",
+    "url": "/kbanach/JavaScript-Helpers/static/js/main.88800d71.chunk.js"
+  },
+  {
+    "revision": "07de565d1acc7021982d",
+    "url": "/kbanach/JavaScript-Helpers/static/js/runtime-main.1335a71a.js"
+  }
+]);
