43245b4a3b06d5dbde72423e9dee0474711d31e4
kbanach <banach.krystian@gmail.com>
2019-11-05 14:21:09 +0100

    Fix buildpath (#4)

---
+++ build/precache-manifest.7121a6ff405a953504a8fa209850c777.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "395dbb8a5ca90110d3028516153419ea",
+    "url": "https://github.com/kbanach/JavaScript-Helpers/index.html"
+  },
+  {
+    "revision": "e4baa2b65f74378e56f6",
+    "url": "https://github.com/kbanach/JavaScript-Helpers/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "36f49bbb6e447d9e3762",
+    "url": "https://github.com/kbanach/JavaScript-Helpers/static/css/main.7015c6b5.chunk.css"
+  },
+  {
+    "revision": "e4baa2b65f74378e56f6",
+    "url": "https://github.com/kbanach/JavaScript-Helpers/static/js/2.7afd7ab5.chunk.js"
+  },
+  {
+    "revision": "36f49bbb6e447d9e3762",
+    "url": "https://github.com/kbanach/JavaScript-Helpers/static/js/main.ae342a08.chunk.js"
+  },
+  {
+    "revision": "8f0a062a741a53b9b2d5",
+    "url": "https://github.com/kbanach/JavaScript-Helpers/static/js/runtime-main.516f3e9e.js"
+  }
+]);
