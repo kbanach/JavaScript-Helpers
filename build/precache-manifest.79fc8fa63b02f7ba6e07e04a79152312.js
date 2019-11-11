26f80595fe4a8e9ba5f84b698d5a0ed7d108cd15
Krystian Banach <banach.krystian@gmail.com>
2019-11-11 16:21:26 +0100

    Fresh build for 1.0.2

--- build/precache-manifest.79fc8fa63b02f7ba6e07e04a79152312.js
+++ build/precache-manifest.79fc8fa63b02f7ba6e07e04a79152312.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "98d1454bc27ec0d4efb29bafb4194d51",
+    "revision": "c8e6496565c09079f3e24378b7837e08",
     "url": "http://kbanach.github.io/JavaScript-Helpers/index.html"
   },
   {
-    "revision": "2a192ccf42caf867c564",
+    "revision": "1d5fc2841a2c646c592a",
     "url": "http://kbanach.github.io/JavaScript-Helpers/static/css/2.0624914a.chunk.css"
   },
   {
-    "revision": "a2225e26ce8609f4c7f3",
+    "revision": "ea79c075291108ca04ad",
     "url": "http://kbanach.github.io/JavaScript-Helpers/static/css/main.7015c6b5.chunk.css"
   },
   {
-    "revision": "2a192ccf42caf867c564",
-    "url": "http://kbanach.github.io/JavaScript-Helpers/static/js/2.7afd7ab5.chunk.js"
+    "revision": "1d5fc2841a2c646c592a",
+    "url": "http://kbanach.github.io/JavaScript-Helpers/static/js/2.7260612c.chunk.js"
   },
   {
-    "revision": "a2225e26ce8609f4c7f3",
-    "url": "http://kbanach.github.io/JavaScript-Helpers/static/js/main.a4ed56a0.chunk.js"
+    "revision": "ea79c075291108ca04ad",
+    "url": "http://kbanach.github.io/JavaScript-Helpers/static/js/main.41e3e6d9.chunk.js"
   },
   {
     "revision": "ecbbda0c23a21b5d077b",
