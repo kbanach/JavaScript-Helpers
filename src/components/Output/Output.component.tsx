9d863ca9bc03f112e2f5e285a80fe67002a0a573
kbanach <banach.krystian@gmail.com>
2021-10-03 21:57:23 +0200

    feat: add copy to cliboard + packages bump (#30)
    * feat: add copy output to clipboard on click (#29)
    
    * Bump ssri from 6.0.1 to 6.0.2 (#24)
    
    Bumps [ssri](https://github.com/npm/ssri) from 6.0.1 to 6.0.2.
    - [Release notes](https://github.com/npm/ssri/releases)
    - [Changelog](https://github.com/npm/ssri/blob/v6.0.2/CHANGELOG.md)
    - [Commits](https://github.com/npm/ssri/compare/v6.0.1...v6.0.2)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump tmpl from 1.0.4 to 1.0.5 (#28)
    
    Bumps [tmpl](https://github.com/daaku/nodejs-tmpl) from 1.0.4 to 1.0.5.
    - [Release notes](https://github.com/daaku/nodejs-tmpl/releases)
    - [Commits](https://github.com/daaku/nodejs-tmpl/commits/v1.0.5)
    
    ---
    updated-dependencies:
    - dependency-name: tmpl
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump path-parse from 1.0.6 to 1.0.7 (#27)
    
    Bumps [path-parse](https://github.com/jbgutierrez/path-parse) from 1.0.6 to 1.0.7.
    - [Release notes](https://github.com/jbgutierrez/path-parse/releases)
    - [Commits](https://github.com/jbgutierrez/path-parse/commits/v1.0.7)
    
    ---
    updated-dependencies:
    - dependency-name: path-parse
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump dns-packet from 1.3.1 to 1.3.4 (#26)
    
    Bumps [dns-packet](https://github.com/mafintosh/dns-packet) from 1.3.1 to 1.3.4.
    - [Release notes](https://github.com/mafintosh/dns-packet/releases)
    - [Changelog](https://github.com/mafintosh/dns-packet/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/mafintosh/dns-packet/compare/v1.3.1...v1.3.4)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump hosted-git-info from 2.8.8 to 2.8.9 (#25)
    
    Bumps [hosted-git-info](https://github.com/npm/hosted-git-info) from 2.8.8 to 2.8.9.
    - [Release notes](https://github.com/npm/hosted-git-info/releases)
    - [Changelog](https://github.com/npm/hosted-git-info/blob/v2.8.9/CHANGELOG.md)
    - [Commits](https://github.com/npm/hosted-git-info/compare/v2.8.8...v2.8.9)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/components/Output/Output.component.tsx
+++ src/components/Output/Output.component.tsx
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { parseComment, parseVar, parseEmptyLine } from './helpers';
 import './Output.css';
 import { OutputComponentProps } from './Output.interface';
+
+async function copyToClipboard(text: string): Promise<void> {
+  navigator.clipboard.writeText(text);
+}
 
 export const OutputComponent: React.FC<OutputComponentProps> = ({
   vars,
   comment,
   settings,
 }) => {
+  const [copiedFlag, setCopiedFlag] = useState<Boolean>(false);
+  const [hideCopiedPopup, setHideCopiedPopup] = useState<Boolean>(false);
+
   const genPrefix = settings.generalPrefix;
   const genPostfix = settings.generalPostfix;
   const emptyLine = parseEmptyLine(settings);
   const parsedVarsLines = vars.map((v) => parseVar(settings, v));
   const parsedComment = parseComment(settings, comment);
 
-  const outputText = [
+  const outputNonEmptyText = [
     genPrefix,
     emptyLine,
     parsedComment,
@@ -23,13 +30,31 @@ export const OutputComponent: React.FC<OutputComponentProps> = ({
     ...parsedVarsLines,
     parsedVarsLines.length > 0 ? emptyLine : '',
     genPostfix,
-  ];
+  ].filter(Boolean);
+
+  function addCopiedPopup() {
+    setHideCopiedPopup(false);
+    setCopiedFlag(true);
+
+    setTimeout(() => {
+      setHideCopiedPopup(true);
+    }, 1000);
+  }
 
   return (
-    <div className="card">
-      <pre className="card-body">
-        <code>
-          {outputText.filter(Boolean).map((l, k) => (
+    <div className="output card">
+      <pre
+        className={`card-body ${copiedFlag ? 'output__copied' : ''} ${
+          hideCopiedPopup ? 'hide-popup' : ''
+        }`}
+      >
+        <code
+          onClick={async () => {
+            await copyToClipboard(outputNonEmptyText.join('\n'));
+            addCopiedPopup();
+          }}
+        >
+          {outputNonEmptyText.map((l, k) => (
             <div key={k}>{l}</div>
           ))}
         </code>
