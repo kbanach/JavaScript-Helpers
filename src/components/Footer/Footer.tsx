714214750141fb85b28cc27bf4c202c78294ba77
Krystian Banach <banach.krystian@gmail.com>
2022-02-12 19:55:37 +0100

    feat: add footer with build time

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import './Footer.css';
+
+
+export const Footer: React.FC = () => (<Row>
+    {process.env.REACT_APP_BUILD_TIME && (<Col className="footer">Built: {process.env.REACT_APP_BUILD_TIME}</Col>)}
+</Row>);
