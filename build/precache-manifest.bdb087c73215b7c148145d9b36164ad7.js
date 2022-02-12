cfdbbc66beec8f86168eced069822b134f0178a3
kbanach <banach.krystian@gmail.com>
2022-02-12 12:30:55 +0100

    chore: add google analytics (#35)
    * chore: add google analytics

--- build/precache-manifest.bdb087c73215b7c148145d9b36164ad7.js
+++ build/precache-manifest.bdb087c73215b7c148145d9b36164ad7.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bbf3b05fc9cea8d96749c66c5d4fae4b",
+    "revision": "93c084b6bdeb0aef1f9eccd3b04aed98",
     "url": "https://kbanach.github.io/JavaScript-Helpers/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "https://kbanach.github.io/JavaScript-Helpers/static/css/2.3b8df3f8.chunk.css"
   },
   {
-    "revision": "e4ea96342f54b6197d7c",
-    "url": "https://kbanach.github.io/JavaScript-Helpers/static/css/main.17a760c8.chunk.css"
+    "revision": "8964882fd6900f640161",
+    "url": "https://kbanach.github.io/JavaScript-Helpers/static/css/main.f7577dfc.chunk.css"
   },
   {
     "revision": "f8d7a0cc4111a6e8cf58",
     "url": "https://kbanach.github.io/JavaScript-Helpers/static/js/2.cccfc14a.chunk.js"
   },
   {
-    "revision": "e4ea96342f54b6197d7c",
-    "url": "https://kbanach.github.io/JavaScript-Helpers/static/js/main.93ddacd4.chunk.js"
+    "revision": "8964882fd6900f640161",
+    "url": "https://kbanach.github.io/JavaScript-Helpers/static/js/main.0c0253ce.chunk.js"
   },
   {
     "revision": "2132fa8bd39523fcb4fd",
