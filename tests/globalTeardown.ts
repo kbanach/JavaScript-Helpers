7e7a2e9f1455510e16a16f526975d2789493b961
czarny <banach.krystian@gmail.com>
2022-06-19 19:05:12 +0200

    test: add removing build dir created for tests

--- tests/globalTeardown.ts
+++ tests/globalTeardown.ts
@@ -1,5 +1,15 @@
+import { stat, rm } from 'fs/promises';
+
 export default async function () {
   console.log('Teardown tests environment');
-  // @ts-ignore
-  globalThis?.server?.close();
+
+  try {
+    // @ts-ignore
+    globalThis?.server?.close();
+
+    await stat('./build'); // checks that build/ exists
+    await rm('./build', { recursive: true, force: true }); // removes build/ dir
+  } catch {
+    // as this is a cleanup, it can fail randomly - in case it's bringing some troubles, this is the place to handle them :)
+  }
 }
